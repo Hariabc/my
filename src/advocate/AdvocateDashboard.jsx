ad893e8e89581b03e7b75ae88ec9e3f57644ebeb
Hariabc <vorugantihariprasad97@gmail.com>
2023-11-16 11:59:06 +0530

    ..

--- src/advocate/AdvocateDashboard.jsx
+++ src/advocate/AdvocateDashboard.jsx
@@ -1,7 +1,28 @@
 import React from 'react'
+import Advocatenavbar from './advocatenavbar'
+import  './advocatenavbar.css'
 
 export default function AdvocateDashboard() {
   return (
-    <div>AdvocateDashboard</div>
-  )
+    <>
+    <ul>
+          <li>
+            <a href="" >Home</a>
+          </li>
+          <li>
+            <a href="">About Us</a>
+          </li>
+          <li>
+            <a href="">Legal Services</a>
+          </li>
+          <li>
+            <a href="">Contact Us</a>
+          </li>
+          <li>
+            <a href="">Login/Register
+            </a>
+          </li>
+        </ul>
+    </>
+    )
 }
---
+++ src/advocate/Advocatenavbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+export default function Advocatenavbar() {
+  return (
+    // <ul>
+    //       <li>
+    //         <a href="">Home</a>
+    //       </li>
+    //       <li>
+    //         <a href="">About Us</a>
+    //       </li>
+    //       <li>
+    //         <a href="">Legal Services</a>
+    //       </li>
+    //       <li>
+    //         <a href="">Contact Us</a>
+    //       </li>
+    //       <li>
+    //         <a href="">Login/Register
+    //         </a>
+    //       </li>
+    //     </ul>
+    <h1>jdso</h1>
+  )
+}
