25599716353cc49fbdde7eff1537a83049865d51
Hariabc <vorugantihariprasad97@gmail.com>
2023-11-20 15:13:52 +0530

    20-11-23

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,72 +1,58 @@
 import React from 'react';
 import oip from '../pages/OIP.jpeg';
 import './Home.css'; // Assuming you have a separate CSS file for styling
-
+/*import fontawesome from "https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css"*/
 export default function Home() {
   return (
-    <>
-      <div className="home-container">
-        <div className="centered-content">
-          <img src={oip} alt="Description of the image" className="centered-image" />
-          <div className="text-and-button">
-            <h3>Unlocking Justice Online</h3> 
-            <p>Your digital path to seamless legal solutions</p>
-            <button>Explore</button>
+    <>  
+      <div>
+        <div className="image-div">
+        <div>
+           <img src="https://c4.wallpaperflare.com/wallpaper/34/747/591/man-made-scales-wallpaper-preview.jpg" className="image1"/>
+        </div>
+        <div className="img-btn-div1">
+        <div className="img-btn-div">
+        <h3 className="image-text">Justice Delayed is Justice Denied</h3>
+        <button class="btn-explore">Explore</button>
+        </div>
+        </div>
+      </div>
+        </div>
+        <div className="boxes">
+          <div className="box1">
+             <h2>Pending cases 10.4M</h2>
+          </div>
+          <div className="box2">
+             <h2>Disposed 10.4M</h2> 
+          </div>
+          <div className="box3">
+             <h2>cases list today 10.4M</h2>
+          </div>
+          <div className="box4">
+             <h2>Number of cases 10.4M</h2>
+          </div>
+          <div className="box5">
+             <h2>District and Taluka court 10.4M</h2>
           </div>
         </div>
-      </div>
+        <div className="ourservices-div">
+          <h1><a href="#" className="services-a">Our Services</a></h1>
+        </div>
+        <div className="ourservicesDiv">
+            <div className="s-1">
+                <h2 className="s-h2">Cause List</h2>
+                <i></i>
+            </div>
+            <div className="s-2">
+                <h2 className="s-h2">Cause List</h2>
+            </div>
+            <div className="s-3">
+                <h2 className="s-h2">Cause List</h2>
+            </div>
+        </div>
+        <div className="footer-div">
 
-      <div class="marque-div">
-        <p class="marque-p">Latest News:</p>
-      </div>
-
-      <div class="boxes-div">
-        {/* Your existing boxes-div content */}
-        <div class="box1">
-          <h4 class="box-header">Number of Wards</h4>
-          <h4 class="box-header">2 M</h4>
         </div>
-        <div class="box2">
-          <h4 class="box-header">Number of Cases</h4>
-          <h4 class="box-header">20 M</h4>
-        </div>
-        <div class="box3">
-          <h4 class="box-header">Disposed Cases</h4>
-          <h4 class="box-header">40 M</h4>
-        </div>
-        <div class="box4">
-          <h4 class="box-header">Number of Cases in Last Month</h4>
-          <h4 class="box-header">1.21 M</h4>
-        </div>
-        <div class="box5">
-          <h4 class="box-header">Number of Cases Listed Today</h4>
-          <h4 class="box-header">65.84 K</h4>
-        </div>
-        <div class="box6">
-          <h4 class="box-header">High Court Complexes</h4>
-          <h4 class="box-header">39</h4>
-        </div>
-        {/* End of existing content */}
-      </div>
-
-      <div class="services-div">
-        <h2 class="services-header">Our Services</h2>
-      </div>
-
-      <div class="service-div2">
-        <div class="services-div1">
-          <div class="service-div">
-            <h4 class="box-header">Case Status</h4>
-          </div>
-          <div class="service-div">
-            <h4 class="box-header">Cause List</h4>
-          </div>
-          <div class="service-div">
-            <h4 class="box-header">e-Judgement</h4>
-          </div>
-        </div>
-      </div>
-
-    </>
-  );
+    </>
+  );
 }
