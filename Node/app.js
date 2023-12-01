67c8baf8b37e1c0e7b548c7a624875a6f9291da3
VenuMadhav21 <venumadhav721@gmail.com>
2023-12-01 16:07:03 +0530

    backend

---
+++ Node/app.js
@@ -0,0 +1,120 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const cors = require("cors");
+app.use(cors());
+app.use(express.json());
+
+
+const mongoUrl = "mongodb+srv://VenuMadhav21:venom21@venu.5szdufj.mongodb.net/Venu?retryWrites=true&w=majority";
+
+mongoose.connect(mongoUrl, {
+    useNewUrlParser:true
+}).then(()=>{console.log("Connected to DataBase");})
+.catch((e)=>console.log(e));
+
+
+
+app.listen(5000 , () => {
+    console.log("Server Started");
+});
+
+// app.post("/post",async(req,res) =>{
+//     console.log(req.body);
+//     const {data} = req.body;
+    
+
+//     try{
+//         if(data == "Venu"){
+//             res.send({status:"ok"})
+//         }
+//         else{
+//             res.send({status:"User Not found"});
+//         }
+
+//     }
+//     catch(error) {
+//          res.send({status: "Error"});
+//     }
+
+
+    
+// });
+
+require("./userDetails");
+
+const User=mongoose.model("Userinfo");
+
+const Advocate = mongoose.model("Advocateinfo");
+
+app.post("/registerClient", async(req,res) => {
+    
+    const { firstName,
+    lastName,
+    username,
+    password,
+    dateOfBirth,
+    email,
+    phoneNumber,
+    address,
+    aadharNumber,
+    gender}= req.body;
+    
+
+    try{
+        await User.create({
+            firstName,
+            lastName,
+            username,
+            password,
+            dateOfBirth,
+            email,
+            phoneNumber,
+            address,
+            aadharNumber,
+            gender,
+        });
+        res.send({status : "ok"})
+
+    } catch(error){
+        res.send({status : "Error"})
+    }
+
+    
+})
+
+app.post("/registerAdvocate", async(req,res) => {
+    
+    const { firstName,
+    lastName,
+    email,
+    phoneNo,
+    licenseNumber,
+    barAssociation,
+    jurisdiction,
+    educationQualifications,
+    yearsOfPractice,
+    practiceArea}= req.body;
+    
+
+    try{
+        await Advocate.create({
+            firstName,
+            lastName,
+            email,
+            phoneNo,
+            licenseNumber,
+            barAssociation,
+            jurisdiction,
+            educationQualifications,
+            yearsOfPractice,
+            practiceArea,
+        });
+        res.send({status : "ok"})
+
+    } catch(error){
+        res.send({status : "Error"})
+    }
+
+    
+})
