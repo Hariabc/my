1b6d47e8923dc703cc796b8184261ea0c39b6281
Hariabc <vorugantihariprasad97@gmail.com>
2023-12-22 12:18:50 +0530

    updated dsahboard

--- src/client/Clientdashboard.jsx
+++ src/client/Clientdashboard.jsx
@@ -1,85 +1,51 @@
 // COAdashboard.js
-import React, { useState, useEffect, useRef } from 'react';
-import "../coa/COAdashboard.css";
-import admin from "../assets/client.png";
+import React from 'react';
+import "./clientdashboard.css";
+import client from "../assets/client.png";
 import { Link } from 'react-router-dom';
 import { IoIosArrowDropdownCircle } from "react-icons/io";
+import { CgProfile } from "react-icons/cg";
+import casefile from "../assets/file.jpg";
+
+const linksData = [
+  { path: "/my-profile", label: "Profile", image: client },
+  { path: "/file-a-case", label: "File a Case", image: casefile },
+  { path: "/pre-trial", label: "Pre Trial", image: null },
+  { path: "/contact-judge", label: "Contact Judge", image: null },
+  { path: "/contact-advocate", label: "Contact Advocate", image: null },
+  { path: "/contact-coa", label: "Contact COA", image: null },
+  { path: "/share-files", label: "Share Files", image: null },
+  { path: "/payment-boxes", label: "Payment Boxes", image: null },
+];
+
 const COAdashboard = () => {
-  const [isDropdownOpen, setIsDropdownOpen] = useState(false);
-  const dropdownRef = useRef(null);
-
-  const toggleDropdown = () => {
-    setIsDropdownOpen(!isDropdownOpen);
-  };
-
-  const closeDropdown = (event) => {
-    if (dropdownRef.current && !dropdownRef.current.contains(event.target)) {
-      setIsDropdownOpen(false);
-    }
-  };
-
-  useEffect(() => {
-    document.addEventListener('click', closeDropdown);
-    return () => {
-      document.removeEventListener('click', closeDropdown);
-    };
-  }, []);
-
   return (
-    <>
+    <div className="client-dashboad">
       <div className='admin'>
         <div className="logo-admin">
-          <img src={admin} alt="ggg" />
+          <img src={client} alt="ggg" />
           <h2>Client</h2>
         </div>
         <div className="admin-head">
           <h1>Welcome to Client dashboard</h1>
         </div>
         <div className="logo-profile">
-          <div className="profile-dropdown" onClick={toggleDropdown} ref={dropdownRef}>
           <IoIosArrowDropdownCircle />
-            <h2>Username</h2>
-            <img src={admin} alt="ggg" />
-            {isDropdownOpen && (
-              <div className="dropdown-content">
-                <Link to="/about">Profile</Link>
-                <a href="#">Settings</a>
-                <a href="#">Logout</a>
-              </div>
-            )}
-          </div>
+          <h2>Username</h2>
+          <img src={client} alt="ggg" />
         </div>
       </div>
-      <div className="sidebar">
-        <div className="section">
-          <h2>Section Title</h2>
-          <ul>
-          <ul>
-            <li><a href="#">Link 1</a></li>
-            <li><a href="#">Link 2</a></li>
-            <li><a href="#">Link 3</a></li>
-            <li><a href="#">Link 4</a></li>
-            <li><a href="#">Link 5</a></li>
-            <li><a href="#">Link 6</a></li>
-            <li><a href="#">Link 7</a></li>
-            <li><a href="#">Link 8</a></li>
-            <li><a href="#">Link 9</a></li>
-            <li><a href="#">Link 10</a></li>
-            <li><a href="#">Link 1</a></li>
-            <li><a href="#">Link 2</a></li>
-            <li><a href="#">Link 3</a></li>
-            <li><a href="#">Link 4</a></li>
-            <li><a href="#">Link 5</a></li>
-            <li><a href="#">Link 6</a></li>
-            <li><a href="#">Link 7</a></li>
-            <li><a href="#">Link 8</a></li>
-            <li><a href="#">Link 9</a></li>
-            <li><a href="#">Link 10</a></li>
-          </ul>
-          </ul>
+      <div className="client-body">
+        <div className="dashboard-boxes">
+          {linksData.map((link, index) => (
+            <Link key={index} to={link.path} className="dashboard-box">
+              {link.image && <img src={link.image} style={{ maxWidth: '100%', height: 'auto' }} />}
+              {link.label}
+            </Link>
+          ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
