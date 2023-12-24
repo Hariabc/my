27175874310a47ebd91831d1230852549192588c
Hariabc <vorugantihariprasad97@gmail.com>
2023-12-24 17:57:40 +0530

    new about

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,16 +1,19 @@
 import React from 'react'
 import "./about.css"
 import ContactUs from './contact'
-
+import {FaArrowRight }from "react-icons/fa"
 export default function About() {
   return (
     <>
     <div className="about">
     <h1>Bringing Efficiency to <span className='justice'>JUSTICE</span></h1>
-    <hr /><br />
+    <hr className="horizontal-line" />
     <p>Our court website is here to make life easier for clients, judges, and advocates. With user-friendly features, we simplify processes, foster collaboration, and enhance the overall efficiency of legal work. Experience a smoother journey in the pursuit of justice with us.</p>
+    <button className="explore-button">
+      Explore
+      <FaArrowRight className="arrow-icon" />
+    </button>
     </div>
-    <h1>About Our Website</h1>
     <ContactUs/>
     </>
   )
