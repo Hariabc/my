70f366e4213e85c123fa55352ea9a204823fa76c
Hariabc <vorugantihariprasad97@gmail.com>
2023-12-24 13:05:17 +0530

    background

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,13 @@
 import React from 'react'
+import "./about.css"
 
 export default function About() {
   return (
+    <>
+    <div className="about">
+
+    </div>
     <h1>About Our Website</h1>
+    </>
   )
 }
