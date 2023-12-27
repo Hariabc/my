015798e2d6945dda4a35dd7c1d70893cc2d145b4
Hariabc <vorugantihariprasad97@gmail.com>
2023-12-27 15:06:11 +0530

    new routes

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,9 @@ import ClientForm from './REGISTER/Client-register';
 import AdvocateForm from './REGISTER/Advocate-register';
 import SetPassword from './components/Setpassword';
 import Advpassword from "./components/privateAdvpassword";
+import PaymentComponent from './dashborad-components/payment.jsx'
+import AdvocateList from './dashborad-components/AdvoacateList.jsx'
+import FileUploader from './dashborad-components/Sendingfiles.jsx'
 
 const App = () => {
   return (
@@ -30,13 +33,15 @@ const App = () => {
           <Route path="/advocate/register" element={<AdvocateForm />} />
           <Route path="/set-password/:token" element={<SetPassword />} />
           <Route path="/Advocate/set-password/:token" element={<Advpassword/>} />
-          
         </Route>
         <Route path="">
           <Route path="/clientdashboard" element={<ClientDashboard />} />
           <Route path="/advocatedashboard" element={<AdvocateDashboard />} />
           <Route path="/judgedashboard" element={<JudgeDashboard />} />
           <Route path="/admindashboard" element={<COADashboard />} />
+          <Route path='/payment' element={<PaymentComponent/>}/>
+          <Route path='/advocatelist' element={<AdvocateList/>}/>
+          <Route path='/sendingfiles' element={<FileUploader/>}/>
         </Route>
       </Routes>
     </Router>
