97f82c02c97a38379388aae18e0aafae7f0cc956
Saiteja9989 <kasojusaiteja10@gmail.com>
2024-01-05 15:39:02 +0530

    9

--- src/components/payment.jsx
+++ src/components/payment.jsx
@@ -16,13 +16,7 @@ const CourtFeePaymentForm = ({ data, onSubmit }) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    try {
-      const response = await axios.post('http://localhost:5000/file/case', data); // Ensure 'data' is available or passed as prop
-      console.log('Form submitted:', response.data);
-      if (onSubmit) onSubmit();
-    } catch (error) {
-      console.error('Error submitting form:', error);
-    }
+    
   };
 
   return (
