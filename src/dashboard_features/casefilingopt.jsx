0e04b3550fe82c20e48b569ab420abf09a1c9f58
Saiteja9989 <kasojusaiteja10@gmail.com>
2024-01-06 21:13:12 +0530

    8

---
+++ src/dashboard_features/casefilingopt.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+import PartyInPersonfrom from './PartyInPerson';
+import PrivateAdvocate from './PrivateAdvocate';
+import PublicAdvocate from './PublicAdvocate';
+
+export default function FileACaseMin() {
+  const [selectedOption, setSelectedOption] = useState(null);
+
+  const handleOptionClick = (option) => {
+    setSelectedOption(option);
+  };
+
+  return (
+    <div>
+      <div>
+        <button onClick={() => handleOptionClick('partyInPerson')}>Party in Person</button>
+        <button onClick={() => handleOptionClick('privateAdvocate')}>Private Advocate</button>
+        <button onClick={() => handleOptionClick('publicAdvocate')}>Public Advocate</button>
+      </div>
+      
+      <div>
+        {selectedOption === 'partyInPerson' && <PartyInPersonfrom />}
+        {selectedOption === 'privateAdvocate' && <PrivateAdvocate />}
+        {selectedOption === 'publicAdvocate' && <PublicAdvocate />}
+      </div>
+    </div>
+  );
+}
