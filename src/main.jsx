ed2c0b0f6ec9349bb379403c9ad1d03ff3fa9249
Saiteja9989 <kasojusaiteja10@gmail.com>
2024-01-06 21:18:38 +0530

    8

--- src/main.jsx
+++ src/main.jsx
@@ -15,6 +15,7 @@ import ClientForm from './REGISTER/Client-register';
 import AdvocateForm from './REGISTER/Advocate-register';
 import SetPassword from './components/Setpassword';
 import Advpassword from "./components/privateAdvpassword";
+import FilecaseOptions from './client_dashboard_features/casefilingopt'
 
 const App = () => {
   return (
