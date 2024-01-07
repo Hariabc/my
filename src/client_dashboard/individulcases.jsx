41e4314e954e00714fe43765469b9679814e9a81
Saiteja9989 <kasojusaiteja10@gmail.com>
2024-01-07 16:02:47 +0530

    9

--- src/client_dashboard/individulcases.jsx
+++ src/client_dashboard/individulcases.jsx
@@ -1,9 +1,7 @@
-// CaseDetails.js
-
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import { useParams } from 'react-router-dom';
-import './CaseDetails.css'
+import './Individualcases.css'
 
 const CaseDetails = () => {
   const { caseId } = useParams();
@@ -21,36 +19,70 @@ const CaseDetails = () => {
 
     fetchCaseDetails();
   }, [caseId]);
-
   return (
-    <div>
-      <h3>Case Details</h3>
+    <div className="container">
+      <h3 className="title">Case Details</h3>
       {caseDetails ? (
         <div>
-          <h4>Case ID: {caseDetails._id}</h4>
-          <h5>Plaintiff Details</h5>
-          <p>Name: {caseDetails.plaintiffDetails.fullName}</p>
-          <p>Gender: {caseDetails.plaintiffDetails.gender}</p>
-          {/* Display other plaintiff details */}
-          <h5>Defendant Details</h5>
-          <p>Name: {caseDetails.defendantDetails.fullName}</p>
-          <p>Gender: {caseDetails.defendantDetails.gender}</p>
-          {/* Display other defendant details */}
-          <h5>Case Details</h5>
-          <p>Case Type: {caseDetails.caseDetails.caseType}</p>
-          <p>Title: {caseDetails.caseDetails.title}</p>
-          {/* Display other case details */}
-          <h5>Payment Details</h5>
-          <p>Payment Method: {caseDetails.paymentDetails.paymentMethod}</p>
-          {/* Display other payment details */}
-          {/* <h5>Document Details</h5>
+          <h4 className="subtitle">Case ID: {caseDetails._id}</h4>
+          <h5 className="subtitle">Plaintiff Details</h5>
+          <div className="details">
+                      <p>Name: {caseDetails.plaintiffDetails.fullName}</p>
+                      <p>Gender: {caseDetails.plaintiffDetails.gender}</p>
+                      <p>Age: {caseDetails.plaintiffDetails.Age}</p>
+                      <p>Email Address: {caseDetails.plaintiffDetails.partyEmailAddresses}</p>
+                      <p>Phonenumber: {caseDetails.plaintiffDetails.partyPhoneNumber}</p>
+                      <p>State: {caseDetails.plaintiffDetails.state}</p>
+                      <p>District: {caseDetails.plaintiffDetails.district}</p>
+                      <p>Village: {caseDetails.plaintiffDetails.village}</p>
+                      
+            
+            {/* Display other plaintiff details */}
+          </div>
+          <h5 className="subtitle">Defendant Details</h5>
+          <div className="details">
+            <p>Name: {caseDetails.defendantDetails.fullName}</p>
+                      <p>Gender: {caseDetails.defendantDetails.gender}</p>
+                      <p>Age: {caseDetails.defendantDetails.age}</p>
+                      <p>Email Address: {caseDetails.defendantDetails.partyEmailAddresses}</p>
+                      <p>Phonenumber: {caseDetails.defendantDetails.partyPhoneNumber}</p>
+                      <p>State: {caseDetails.defendantDetails.state}</p>
+                      <p>District: {caseDetails.defendantDetails.district}</p>
+                      <p>Village: {caseDetails.defendantDetails.village}</p>
+          
+
+            {/* Display other defendant details */}
+          </div>
+          <h5 className="subtitle">Case Details</h5>
+          <div className="details">
+            <p>Case Type: {caseDetails.caseDetails.caseType}</p>
+                      <p>Title: {caseDetails.caseDetails.title}</p>
+                      <p>Case Summary: {caseDetails.caseDetails.caseSummary}</p>
+                      <p>Cause of Action: {caseDetails.caseDetails.causeOfAction}</p>
+                      <p>Date of cause of Action: {caseDetails.caseDetails.dateOfCauseOfAction}</p>
+                      <p>Relief Sought: {caseDetails.caseDetails.reliefsought}</p>
+                      <p>Court Name: {caseDetails.caseDetails.courtName}</p>
+                      <p>Court District: {caseDetails.caseDetails.courtDistrict}</p>
+            <p>Court State: {caseDetails.caseDetails.courtState}</p>
+                      
+            {/* Display other case details */}
+          </div>
+          <h5 className="subtitle">Payment Details</h5>
+          <div className="details">
+                      <p>Payment Method: {caseDetails.paymentDetails.paymentMethod}</p>
+                      {/* <p>Payment Method: {caseDetails.paymentDetails.paymentMethod}</p>
+                      <p>Payment Method: {caseDetails.paymentDetails.paymentMethod}</p>
+                      <p>Payment Method: {caseDetails.paymentDetails.paymentMethod}</p> */}
+            {/* Display other payment details */}
+          </div>
+          {/* <h5 className="subtitle">Document Details</h5>
           {caseDetails.documents.map((document, index) => (
             <p key={index}>Document {index + 1}: {document}</p>
-          ))} */}
-          {/* Display other document details */}
+          ))}
+          Display other document details */}
         </div>
       ) : (
-        <p>Loading...</p>
+        <p className="loading">Loading...</p>
       )}
     </div>
   );
