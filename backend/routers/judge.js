a688b64fe532b86d59d86b2bb0564e249ae704fb
VenuMadhav21 <venumadhav721@gmail.com>
2024-01-19 20:57:08 +0530

    update

--- backend/routers/judge.js
+++ backend/routers/judge.js
@@ -311,7 +311,7 @@ const nodemailer = require('nodemailer');
 
   router.post('/create-conference', authMiddleware, async (req, res) => {
     try {
-      const { caseNumber, plaintiffName, defendantName, advocateName, title, description, date, meetingID } = req.body;
+      const { caseNumber, plaintiffName, defendantName, advocateName, title, description, date, meetingID,conferenceType} = req.body;
       const userId = req.user._id;
   
       // Create a new conference
@@ -324,6 +324,7 @@ const nodemailer = require('nodemailer');
         description,
         date,
         meetingID,
+        conferenceType,
         user: userId,
       });
   
@@ -332,6 +333,30 @@ const nodemailer = require('nodemailer');
   
       // Update the case status to "preTrialconferenceScheduled"
       await Case.findOneAndUpdate({ caseNumber }, { $set: { caseStatus: 'preTrialconferenceScheduled' } });
+
+      const hearingId = Math.floor(Math.random() * 100000);
+
+      const hearingData = {
+        hearingId,
+        hearingDate: new Date(date), // Use the scheduled conference date as the hearing date
+        hearingTime: '', // You might want to set this based on your application logic
+        hearingMode: conferenceType, // Use the scheduled conference type as the hearing mode
+        hearingStatus: 'scheduled',
+        hearingNotes: '', // Leave it blank for now
+      };
+  
+      // Create a new hearing
+      const newHearing = new Hearing(hearingData);
+  
+      // Save the new hearing
+      await newHearing.save();
+  
+      // Associate the hearing with the conference
+      newConference.hearing = newHearing._id;
+  
+      // Save the updated conference with the hearing reference
+      await newConference.save();
+  
   
       res.status(201).json({ message: 'Conference created successfully', data: newConference });
     } catch (error) {
